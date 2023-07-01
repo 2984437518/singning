76959d0a69c8d9a655e0481d142cc446de7f500e
2984437518 <127410382+2984437518@users.noreply.github.com>
2023-07-01 14:21:15 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,51 @@
+hhh()
+function hhh(){
+    var shuru = ""
+while(shuru != "singning2000552588sing391509263372"){
+shuru = prompt("请输入卡密进入软件");
+}
+alert("欢迎光临");
+alert("星宁欢迎您的光临");
+ }
+function sheng(){
+let put = document.getElementById("put").value;
+if(put == ""){
+alert("未输入跑号");
+      v1.innerHTML="遇到了错误请重试"
+}else{
+v1.innerHTML="正在生成链接/兑换码"
+setTimeout(function(){
+            var a = [
+
+"http://test.cashbook.work/7ee40347325343c3af8bbdef444ff45f",
+"http://test.cashbook.work/0f8c694da7414f6e9e452fcf87d54e1c",
+"http://test.cashbook.work/0c2ee3f7781746fbb89b1a7f49a12d58",
+"http://test.cashbook.work/cfd59873251f445297bf0a265d09a72b",
+"http://test.cashbook.work/98a71cbafd804a8ca9bf4bd8bfecc97a",
+"http://192.74.254.158:5272/kcxip5aw2ic39",
+"http://192.74.254.158:5272/isd0hn8ji06s1",
+"http://test.cashbook.work/af9c5508eacc4f89a5496e7369568d77",
+"http://test.cashbook.work/8e37ad5758574b11a5707f7007557f46",
+"http://test.cashbook.work/caca7c15fd8b47e7ae1eca6a4190ae70",
+"http://test.cashbook.work/743f914be6874da295a89cc9a38100da",
+"http://test.cashbook.work/92dc926c1e73401a9af3711709b459a3",
+"http://test.cashbook.work/70d149ae029846a6972308cb7e3f8057",
+"http://test.cashbook.work/274f76a49b0f46a9b095a2434525549a",
+"http://test.cashbook.work/f110a421ddcd477ea1e1f388beadc197",
+"http://test.cashbook.work/b417783d75be4a8c8d098136f59e23e0",
+"http://test.cashbook.work/faeff8099a024072a8dc0061eae50d01",
+"http://test.cashbook.work/51f858448e134349b6d26e499b54defc",
+"http://test.cashbook.work/7bd5edfab32148939e588246763e51de",
+"http://test.cashbook.work/a1fc8681a4ed4775a59c78a019310bd3"
+]
+            var random=parseInt(Math.random()*7+0)
+            v1.innerHTML=a[random]
+},3000);
+
+
+}
+
+}
+function fu(){
+alert("请在下方复制");
+}
